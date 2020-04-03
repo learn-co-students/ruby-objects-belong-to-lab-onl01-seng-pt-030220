2eea78e6bfab8cb29b72909911e23f5aa5d2d4cb
Doug Jones <dsjr1284@gmail.com>
2020-04-03 23:53:05 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+attr_accessor :title, :artist
+
+end
+
+
+
+# class Artist
+#   attr_accessor :name, :genre
+#
+#   def initialize(name, genre)
+#     @name = name
+#     @genre = genre
+#   end
+#
+# end
